bc9cb53911594b6f21b142e8a04d809707c25bd9
Jan Kozłowski <jasiokzl@gmail.com>
2025-12-08 03:01:51 +0100

    Detection algorithms and pipeline

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from ClassificationModel import ClassificationRandomForest
 from ClusterSVM import ClusterSVM
 
+import detectors.byHough as hough
+import detectors.byApproxPolyDP as approxPolyDP
+import detectors.bySegmentation as segmentation
 
 def train_svm():
     model = ClusterSVM(target_class='rectangle', img_size=(64, 64))
@@ -27,21 +30,73 @@ def train_rf():
     print("Training metrics:", metrics)
     model.save_model('models/rf_rectangle_model.joblib')
 
-def test_svm():
+def test_svm(image_path, shape):
+    model_path = f"models/binary_{shape}_model.joblib"
     model = ClusterSVM(target_class='triangle', img_size=(64, 64))
-    model.load_model('models/binary_triangle_model.joblib')
+    model.load_model(model_path)
 
-    y_pred = model.predict('input/rectangle/I-38/001_201311251210402_5901_celje.png')
-    print("Predicted label:", y_pred)
+    return model.predict(image_path)
 
-def test_rf():
-    model = ClassificationRandomForest(data_dir='input/circle', img_size=(64, 64))
-    model.load_model('models/rf_circle_model.joblib')
+def test_rf(image_path, shape):
+    model_path = f"models/rf_{shape}_model.joblib"
+    model = ClassificationRandomForest(data_dir='cropped/', img_size=(64, 64))
+    model.load_model(model_path)
 
-    y_pred = model.predict('input/circle/II-3/001_201311071110100_13511_celje.png')
-    print("Predicted label:", y_pred)
+    return model.predict(image_path)
+
+
+def main():
+    image_path = "JPEGImages/0000289.jpg"
+    all_crops = []
+
+    modules  = [
+        ("hough", hough),
+        ("approx", approxPolyDP),
+        ("segmentation", segmentation)
+    ]
+
+    for name, mod in modules:
+        print(f"Running {name}...")
+        try:
+            crops = mod.run_detector(image_path)
+            all_crops.extend(crops)
+        except Exception as e:
+            print(f"Error in {name}:", e)
+
+    print("\nDETECTED SIGNS: ")
+    printed_labels = set()
+
+    for filename in all_crops:
+
+        if "circle" in filename or "octagon" in filename:
+            shape = "circle"
+        elif "triangle" in filename:
+            shape = "triangle"
+        elif "square" in filename or "rectangle" in filename:
+            shape = "rectangle"
+        else:
+            shape = "unknown"
+
+        if shape == "unknown":
+            continue
+
+        label = test_rf(filename, shape)
+
+        if label.lower() == "X-1.2" or label in printed_labels:
+            continue
+
+        print(label)
+        printed_labels.add(label)
+
+
+
+
 
 if __name__ == "__main__":
+    # train_rf()
     # train_svm()
-    train_rf()
+
+    # test_svm()
     # test_rf()
+
+    main()
